c62f7ea315e54c8768a90f27ce7ade31a90411d0
github username <github email address>
2018-11-14 15:35:26 -0700

    start styling deck cards on home page

--- client/src/components/Decks.js
+++ client/src/components/Decks.js
@@ -2,20 +2,32 @@ import React from 'react'
 import { Switch, Route } from 'react-router-dom'
 import Deck from './presentational/Deck'
 
-const Decks = () => {
+class Decks extends React.Component{
+  state = {
+    value: ''
+  }
+
+  handleChange = e =>{
+    this.setState({
+      value: e.target.value
+    })
+  }
+
+  render(){
   return(
-    <div>
-    <h1>102<br></br> Study Sets and Counting.</h1>
-    <Deck />
-    <Deck />
-    <Deck />
-    <Deck />
-    <Deck />
-    <Deck />
-    <Deck />
-    <Deck />
+    <div className="decks-container">
+      <h1>102<br></br> Study Sets and Counting.</h1><br></br>
+      <div className="card-deck">
+        <Deck value={this.state.value} onChange={this.handleChange}/>
+        <Deck value={this.state.value} onChange={this.handleChange}/>
+        <Deck value={this.state.value} onChange={this.handleChange}/>
+        <Deck value={this.state.value} onChange={this.handleChange}/>
+        <Deck value={this.state.value} onChange={this.handleChange}/>
+        <Deck value={this.state.value} onChange={this.handleChange}/>
+        <Deck value={this.state.value} onChange={this.handleChange}/>
+      </div>
     </div>
-  )
+  )}
 }
 
 export default Decks
--- client/src/components/presentational/Deck.js
+++ client/src/components/presentational/Deck.js
@@ -1,8 +1,23 @@
 import React from 'react'
 
-const Deck = () => {
+const Deck = (props) => {
   return(
-    <div>Deck </div>
+    <div className="deck-div">
+      <header className="top-info">
+        <span>32 terms</span> <span className="username">Username</span>
+      </header>
+      <h4>Deck Name</h4>
+      <footer>
+        <button className="enter-password">lock</button>
+        <input type="text" placeholder="enter password..." value={props.value} onChange={props.onChange}/>
+        <section className="left-deck-buttons">
+          <button className="copy-deck">copy</button>
+          <button className="save-deck">save</button>
+
+        </section>
+    </footer>
+
+    </div>
   )
 }
 
