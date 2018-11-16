6a95e9430364258c1263c82aa723ebfb2603c1af
github username <github email address>
2018-11-15 17:36:22 -0700

    style DeckPage -- style header, TopScores and start Question styling

--- client/src/components/presentational/DeckPage.js
+++ client/src/components/presentational/DeckPage.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import TopScore from './TopScore'
+import Question from './Question'
 
 const topscores= [{time: 103, score: 2, username: "Savannah"}, {time: 103, score: 2, username: "sav"}, {time: 103, score: 2, username: "sav"}]
 const DeckPage = (props) => (
   <div>
-    <header>
+    <div className="overlay"></div>
+    <header className="deck-page-header">
       <section className="top-info">
         <span>32 terms</span> <span className="username">HighScore:59</span>
         <h1>Deck Name</h1>
@@ -13,14 +15,14 @@ const DeckPage = (props) => (
       </section>
 
       <section className="highscores">
-        <p>Top Scores:</p>
+        <h6 className="topscores">Top Scores:</h6>
         {topscores.map((round, index) => <TopScore key={index} round={round} num={index+1}/>)}
       </section>
     </header>
 
-
-
-
+    <main className='deck-body'>
+      <Question />
+    </main>
 
   </div>
 )
