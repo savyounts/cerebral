0a2c4b5bc6c29f17e82021782badb5055c040b4e
github username <github email address>
2018-11-17 11:33:20 -0700

    create CardForm and base form within CreateDeck

--- client/src/components/CreateDeck.js
+++ client/src/components/CreateDeck.js
@@ -1,18 +1,46 @@
 import React from 'react'
+import CardForm from './CreateDeck/CardForm'
 
-const CreateDeck = () => {
+class CreateDeck extends React.Component {
+  state = {
+    name: '',
+    cards: []
+  }
+
+  handleChange = e =>{
+    this.setState({
+      name: e.target.value
+    })
+  }
+
+  handleClick = e => {
+    let newCard = <CardForm />
+    let cards = this.state.cards.unshift(newCard);
+    this.setState({cards});
+  }
+
+  renderCards(){
+    return this.state.cards.map((cards, index) => {
+      return <CardForm key={index} />;
+   });
+  }
+
+  render(){
   return(
+    <React.Fragment>
     <h1>Make a new deck</h1>
-//     <form>
-//       <input type="text" name="question" value={this.state.name} onChange={this.onChange}/>
-//
-//       <input type="text" name="answer" value={this.state.name} onChange={this.onChange}/>
-//
-//       <input type="text" name="hint" value={this.state.name} onChange={this.onChange}/>
-//
-//       <input type="text" value="create deck"/>
-//
-  )
+    <form>
+      <input type="text" value={this.state.name} onChange={this.handleChange} placeholder="Name Your Deck"/>
+      <br></br>
+      {this.state.cards}
+      {this.renderCards()}
+      <br></br>
+      <input type="submit" value="create deck"/>
+    </form>
+    <button onClick={this.handleClick}> + ADD CARD </button>
+  </React.Fragment>
+  )}
 }
+// {this.state.addCard ? <CardForm /> : <React.Fragment></React.Fragment>}
 
 export default CreateDeck
