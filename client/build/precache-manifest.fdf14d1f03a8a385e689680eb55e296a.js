e954a18555e3591b70c530dc5d168e8026050ce2
github username <github email address>
2019-02-05 15:25:33 -0700

    add server.js with scripts, change client gitignore to get rready for heroku deployment

---
+++ client/build/precache-manifest.fdf14d1f03a8a385e689680eb55e296a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "76dfc3de43c5038c69c14bd946bfaece",
+    "url": "/static/media/home.76dfc3de.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0fcff24d6fa7ebe6e922",
+    "url": "/static/js/main.0fcff24d.chunk.js"
+  },
+  {
+    "revision": "52fc3aef4aabd0a4a34e",
+    "url": "/static/js/1.52fc3aef.chunk.js"
+  },
+  {
+    "revision": "0fcff24d6fa7ebe6e922",
+    "url": "/static/css/main.a9066ddd.chunk.css"
+  },
+  {
+    "revision": "52fc3aef4aabd0a4a34e",
+    "url": "/static/css/1.c90a5b53.chunk.css"
+  },
+  {
+    "revision": "57d8cd8748a23e1fabd90f05497ab922",
+    "url": "/index.html"
+  }
+];
